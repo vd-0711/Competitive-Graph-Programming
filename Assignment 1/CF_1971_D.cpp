b981cbe5851773beab67fc04a1cdaaaf1a1cbb73
Vidhi Chordia <94589050+vd-0711@users.noreply.github.com>
2024-05-24 23:48:11 +0530

    Update CF_1971_D.cpp

--- Assignment 1/CF_1971_D.cpp
+++ Assignment 1/CF_1971_D.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int binaryCuts(string s) {
+    int n = s.length();
+    vector<int> arr(n + 1, INT_MAX);
+    int ct = 0;
+    for (int i = 0; i <n; ++i) {
+        if (s[i] == '1' && s[i+1] =='0') {
+                ct++;
+            }
+        }
+    return ct;
+}
+
+int main() {
+    string s;
+    int t;
+    cin >> t ;
+    while(t--){
+        cin >> s ;
+        int result = binaryCuts(s);
+        cout << result +1 << endl;
+    }
+    return 0;
+}
